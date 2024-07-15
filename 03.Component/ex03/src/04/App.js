b134b5efb5033af695f37fe0b704fa70436dc7a9
leejw-lu <jiwoojenny@naver.com>
2024-07-15 17:07:08 +0900

    class: 컴포넌트(3) Event Handler 예제

--- 03.Component/ex03/src/04/App.js
+++ 03.Component/ex03/src/04/App.js
@@ -1,22 +1,90 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import logo from './assets/images/react-logo.png';
 
 export default function App() {
+
+    const imgRef= useRef(null);
+
+    const onKeyPressInput= (e) => {
+        if (e.key="Enter") {
+            console.log("KeyPress: "+ e.target.value);
+        }
+
+    };
+
+    const onChangeInput= (e) => {
+        console.log("Changed" + e.target.value);
+    };
+
+    const onFocusInput = () => {
+        console.log("Focused");
+    }
+
+    const onBlurInput = () => {
+        console.log("Blur");
+    }
+
+    const onMouseOverImg = (e) => {
+        const offsetTop = imgRef.current.offsetTop;
+        const offsetLeft= imgRef.current.offsetLeft;
+        
+        //console.log("Mousemove", `x=${e.clientX}, y=${e.clientY}`);
+        console.log("Mouseover", `x=${e.clientX- offsetLeft}, y=${e.clientY-offsetTop}`);
+    }
+
+    const onMouseMoveImg = (e) => {
+        console.log("Mousemove", `x=${e.clientX}, y=${e.clientY}`);
+    }
+
+    const onMouseOutImg = (e) => {
+        console.log("Mouseout", `x=${e.clientX}, y=${e.clientY}`);
+    }
+
+    const onMouseDownImg = (e) => {
+        console.log("Mousedown", `x=${e.clientX}, y=${e.clientY}`);
+    }
+
+    const onMouseUpImg = (e) => {
+        console.log("MouseUp", `x=${e.clientX}, y=${e.clientY}`);
+    }
+
+    const onClickImg = (e) => {
+        console.log("onclick", `x=${e.clientX}, y=${e.clientY}`);
+    }
+
+    const onDoubleClickImg = (e) => {
+        console.log("dbclick", `x=${e.clientX}, y=${e.clientY}`);
+    }
+
     return (
         <>
             <h2>Event Handler  예제</h2>
             <input
                 type='text'
-                placeholder='메세지를 입력 하세요' />
+                placeholder='메세지를 입력 하세요'
+                onKeyPress={onKeyPressInput}
+                onChange={onChangeInput}
+                onFocus={onFocusInput} 
+                onBlur={onBlurInput}
+                />
                 <br/>
                 <br/>
             <img
+                ref={imgRef}
                 style={ {
                     cursor: 'pointer',
                     width: 190,
                     border: '1px solid #ccc'
                 } }
-                src={logo} />
+                src={logo} 
+                onMouseOver ={onMouseOverImg} 
+                onMouseMove = {onMouseMoveImg}
+                onMouseOut= {onMouseOutImg}
+                onMouseDown= {onMouseDownImg}
+                onMouseUp= {onMouseUpImg}
+                onClick= {onClickImg}
+                onDoubleClick= {onDoubleClickImg}
+                />
         </>
     );
 }
