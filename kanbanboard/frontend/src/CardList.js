df0269bee7915040502202a8fd153136dc0aece3
leejw-lu <jiwoojenny@naver.com>
2024-07-17 19:54:08 +0900

    assignment: kanbanboard 리팩토링

--- kanbanboard/frontend/src/CardList.js
+++ kanbanboard/frontend/src/CardList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Card from './Card';
 import {Card_List} from './assets/scss/CardList.scss';
 
-function CardList({boards}) {
+function CardList({boards, status}) {
     //console.log(boards[0].status);
     //boards의 배열 길이에 따라 Cards 만큼
     return (
         <div className={Card_List}>
-            <h1>{boards[0].status}</h1>
+            <h1>{status}</h1>
             {boards.map(e=> <Card key={e.no} no={e.no} title={e.title} description={e.description} tasks={e.tasks}  />) }
         </div>
     );
--- kanbanboard/frontend/src/KanbanBoard.js
+++ kanbanboard/frontend/src/KanbanBoard.js
@@ -4,24 +4,27 @@ import './assets/scss/KanbanBoard.scss';
 import boards from './assets/json/data';
 
 function KanbanBoard() {
+    /* return 안에서 처리할 수 있으면 최대한 바깥보다 return 안에서 처리하도록.
+
     const TodoList = boards.filter(data => (
         data.status === "ToDo"				
     ));
-
     const DoingList = boards.filter(data => (
         data.status === "Doing"				
     ));
-
     const DoneList = boards.filter(data => (
         data.status === "Done"				
-    ));
+    ));*/
 
     return (
         <div className={'Kanban_Board'}>
-            {/*TodoList.map(e=> <CardList key={e.no} no={e.no} title={e.title} description={e.description} status={e.status} tasks={e.tasks}  />) */}
-            <CardList boards={TodoList}/>
-            <CardList boards={DoingList}/>
-            <CardList boards={DoneList} />
+            <CardList boards={boards.filter(data => ( data.status === "ToDo"))} status={"ToDo"}/>
+            <CardList boards={boards.filter(data => ( data.status === "Doing"))} status={"Doing"}/>
+            <CardList boards={boards.filter(data => ( data.status === "Done"))} status={"Done"}/>
+
+            {/* <CardList boards={TodoList} status={"ToDo"}/>
+            <CardList boards={DoingList} status={"Doing"}/>
+            <CardList boards={DoneList} status={"Done"}/> */}
         </div>
     );
 }
