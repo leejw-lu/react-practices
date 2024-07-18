9aa3b12ee6fd01cd41174139cd4194e59b1014d5
leejw-lu <jiwoojenny@naver.com>
2024-07-18 13:57:53 +0900

    class: emaillist8 front api 연결

--- emaillist08/frontend/src/App.js
+++ emaillist08/frontend/src/App.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import RegisterForm from './RegisterForm';
 import Searchbar from './Searchbar';
 import Emaillist from './Emaillist';
 import './assets/scss/App.scss';
 
 function App() {
-    const emails= [
-        {no:1, name:'둘리', email:'dooly@gmail.com'},
-        {no:2, name:'마이콜', email:'michol@gmail.com'},
-        {no:3, name:'도우너', email:'douner@gmail.com'},
-        {no:4, name:'또치', email:'ddochi@gmail.com'}    
-    ];
+    const [emails, setEmails] = useState(null);
+
+    const fetchEmails=async (keyword) => {
+        try{
+            const response= await fetch(`/api?kw=${keyword ? keyword: ''}`, {
+                method: 'get',
+                headers: {
+                    'Accept' : 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: null
+            });
+
+            if(!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+
+            const json = await response.json();
+
+            if(json.result !== 'success'){
+                throw new Error(json.message);
+            }
+
+            setEmails(json.data);
+
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(()=> {
+        fetchEmails();
+    }, []);
 
     return (
         <div id={'App'} >
             <RegisterForm />
-            <Searchbar/>
+            <Searchbar fetchEmails={fetchEmails}/>
             <Emaillist emails={emails}/>
         </div>
     );
--- emaillist08/frontend/src/EmailItem.js
+++ emaillist08/frontend/src/EmailItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Email_Item} from './assets/scss/EmailItem.scss';
 
-function EmailItem({name, email}) {
+function EmailItem({firstName, lastName, email}) {
     return (
         <li className={Email_Item}>
-        {name}
-        <br/>
-        {email}
+            <h4>{firstName}{lastName}</h4>
+            <span>{email}</span>
+            <a href='' />
         </li>
     );
 }
