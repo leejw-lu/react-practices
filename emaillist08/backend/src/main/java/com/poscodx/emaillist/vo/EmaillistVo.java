ccd50e9dd8a3a6d2f90090dde684400dfe423190
leejw-lu <jiwoojenny@naver.com>
2024-07-18 13:58:48 +0900

    class: emaillist8 backend init 및 api 연결

---
+++ emaillist08/backend/src/main/java/com/poscodx/emaillist/vo/EmaillistVo.java
@@ -0,0 +1,15 @@
+package com.poscodx.emaillist.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class EmaillistVo {
+	private Long no;
+	private String email;
+	private String firstName;
+	private String lastName;
+}
