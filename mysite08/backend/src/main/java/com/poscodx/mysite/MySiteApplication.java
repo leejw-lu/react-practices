e814f40a47cd98707817db1e10c2773f03d3ea40
leejw-lu <jiwoojenny@naver.com>
2024-07-19 21:14:13 +0900

    class: mysite8 BE init

---
+++ mysite08/backend/src/main/java/com/poscodx/mysite/MySiteApplication.java
@@ -0,0 +1,27 @@
+package com.poscodx.mysite;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MySiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySiteApplication.class, args);
+	}
+
+	// comment by jiwoo
+	@Bean
+	public TomcatServletWebServerFactory servletContainer() {
+		return new TomcatServletWebServerFactory() {
+			@Override
+			protected void customizeConnector(Connector connector) {
+				super.customizeConnector(connector);
+				connector.setParseBodyMethods("POST,PUT,DELETE");
+			}
+		};
+	}
+}
